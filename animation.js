dc366ec96c22ab6fa51509c2dcd5ff920b41a1cb
Olivia Styler <olivia.styler@utah.edu>
2026-01-20 20:00:37 -0700

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,13 @@
+$("#orderHeader").click(function(){
+                $("#orderHidden").slideToggle('2000',"swing");
+              
+            });
+$("#sideHeader").click(function(){
+  $("#sideHidden").slideToggle('1000',"swing");
+              
+            });
+$("#dessertHeader").click(function(){
+  $("#dessertHidden").slideToggle('1000',"swing");
+              
+            });
+console.log("did this even load");
---
+++ difanimation.js
@@ -0,0 +1,18 @@
+document.getElementById("nav__links").addEventListener("click", function(e) {
+  const tgt = e.target;
+    tgt.classList.toggle('open');
+    console.log("is this working?");
+    // here you can test tgt.classList.contains('open') to see the state
+});
+document.getElementById("nav__links2").addEventListener("click", function(e) {
+  const tgt = e.target;
+    tgt.classList.toggle('open');
+    console.log("is this working?");
+    // here you can test tgt.classList.contains('open') to see the state
+});
+document.getElementById("nav__links3").addEventListener("click", function(e) {
+  const tgt = e.target;
+    tgt.classList.toggle('open');
+    console.log("is this working?");
+    // here you can test tgt.classList.contains('open') to see the state
+});
